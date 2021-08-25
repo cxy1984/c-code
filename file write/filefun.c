82e735582e4d0bdea48cf50adb87bfc0dae0d679
ayuayue <1401262639@qq.com>
2021-08-25 22:48:45 +0800

    add gitignore

--- file write/filefun.c
+++ file write/filefun.c
@@ -30,14 +30,9 @@ int main(void)
     else 
     {
         printf("文件创建并打开成功\n");
-        // fwrite(node,6, 1, fp);
         fwrite(node->name, strlen(node->name), 1, fp); // sizeof 获取内存长度，变量占用的字节数，strlen
         fwrite(node->number, strlen(node->number), 1, fp);
-        //fread(node,sizeof(struct student),1,fp);//判断写入的内容
-        // fwrite(&content, sizeof(content), 10, fp);
-        // printf("写入的内容是：\n");
-        // printf("姓名：%s\n",node->name);
-        // printf("学号：%d\n",node->number);
+
         fclose(fp);
     }
     
