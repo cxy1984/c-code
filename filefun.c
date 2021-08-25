b6181435646fbd03e7393bc2841c5d79bbbf2c23
MIyanx <68494458+cxy1984@users.noreply.github.com>
2021-08-25 16:54:09 +0800

    增加了注释
    对结构体初始化的内容保存到一个临时创建的文本文档中

--- filefun.c
+++ filefun.c
@@ -6,18 +6,18 @@ struct student
     char name[20];
     int number;
 };
-void input(struct student *node)
+void input(struct student *node)//用scanf对结构体初始化
 {
     printf("请输入姓名：");
     scanf("%s",node->name);
     printf("请输入学号：");
-    scanf("%d",&node->number);
+    scanf("%d",&node->number);//注意读入格式
 
 }
 int main(void)
 {   
     struct student *node=NULL;
-    node=(struct student*)malloc(sizeof(struct student));
+    node=(struct student*)malloc(sizeof(struct student));//结构体指针要申请内存空间才能使用
     input(node);
     printf("这个学生的信息是：%s %d\n",node->name,node->number);
     FILE *fp;
@@ -28,12 +28,10 @@ int main(void)
     {
         printf("文件创建并打开成功\n");
         fwrite(node,sizeof(struct student),1,fp);
-        fread(node,sizeof(struct student),1,fp);
+        fread(node,sizeof(struct student),1,fp);//判断写入的内容
         printf("写入的内容是：\n");
         printf("姓名：%s\n",node->name);
         printf("学号：%d\n",node->number);
-
-        //if()//判断是否写入成功
         fclose(fp);
     }
     return 0;
